a899cae5f5714e5c910c71f51fe76d7d42e11f3d
Brandon Nightingale <brandon@jbnight.com>
2020-02-09 20:34:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,13 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else
+  elsif phrase == "Hi Nana, how are you?"  
     return "HUH?! SPEAK UP, SONNY"
+  elsif phrase == "Hi!"
+    return "HUH?! SPEAK UP, SONNY"
+  elsif phrase == "WHAT?"
+    return "NO, NOT SINCE 1938!"
+  elsif phrase == "WHAT DID YOU EAT TODAY?"
+    return "NO, NOT SINCE 1938!"
   end
 end
